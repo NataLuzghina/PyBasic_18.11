3d1e30a7d1d3f346e08d56f2a129ede0038354e4
NataLuzghina <princessa.wuki@gmail.com>
2022-11-27 13:54:39 +0200

    Home work_1

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('hello')
+variable = '\t\timport antigravity'
+print(variable)
+variable = '\t\timport_hello_'
+print(variable)
+variable = '\t\tfrom_future_import bracers'
+print(variable)
+name = '*\t\t*\t\t *\t\t*******\t\t*\t\t\n' \
+       '**\t\t*\t    * *\t\t   *\t   * *\t   \n' \
+       '* *\t\t*\t   *   *\t   *\t  *   *\t  \n' \
+       '*  *\t*\t  *\t    *\t   *\t *\t   *\t \n' \
+       '*   *\t*\t *\t\t *\t   *\t*\t\t*\t\n' \
+       '* \t *  *   ***********\t   *   ***********   \n' \
+       '* \t  * *  *\t\t   *   *  *\t\t\t  *  \n' \
+       '*\t    * *\t\t\t\t*  * *\t\t\t   *'
+print(name)
+str1 = '\\a      Bell (alert)'
+print(str1)
+str2 = '\\b      Backspace'
+print(str2)
+str3 = '\\n      New line'
+print(str3)
+str4 = '\\t      Horizontal tab'
+print(str4)
+
